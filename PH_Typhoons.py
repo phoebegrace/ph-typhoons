ea59763bc5497d18ea386645f701aaf9971fd3e4
Phoebe Grace Juayong <99640768+phoebegrace@users.noreply.github.com>
2024-04-22 13:50:06 +0800

    Update PH_Typhoons.py

--- PH_Typhoons.py
+++ PH_Typhoons.py
@@ -16,7 +16,7 @@ if "model" not in st.session_state:
 def app():
     st.subheader('RNN-LSTM Based Typhoon Prediction in the Philippines')
     
-    text = """Prof. Louie F. Cervantes, M. Eng. (Information Engineering)
+    text = """Phoebe Grace C. Juayong, BSCS 3A
     \nCCS 229 - Intelligent Systems
     *Department of Computer Science
     *College of Information and Communications Technology
